b1bd55d3cbc261cc7289f5cb440b2bfd4cc3716c
santosbuenoemanuel-svg <santos.bueno.emanuel@escola.pr.gov.br>
2026-09-17 11:42:46 -0300

    Create aleatório.js

---
+++ aleatório.js
@@ -0,0 +1,9 @@
+const nomes = ["Fernanda", "Jõao", "Jorge", "Fabrício", "Felipe", "Julia", "Julho"]
+
+export function Aleatório (Lista){
+    const posição = Math.floor(Math.random()* lista.length);
+    return Lista [posição];
+}
+
+export const nome = aleatório(nomes)
+  
